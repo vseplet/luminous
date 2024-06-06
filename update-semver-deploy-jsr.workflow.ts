419efd0edc7ab4aa7b585fa79a3c5c02cf4d2604
Vsevolod Pletnev <vseplet@gmail.com>
2024-06-07 00:04:06 +0700

    fix 8

--- update-semver-deploy-jsr.workflow.ts
+++ update-semver-deploy-jsr.workflow.ts
@@ -74,7 +74,7 @@ const workflow = core.workflow(UpdateVersionContext)
           );
 
           newVersionsTS = `export default [ ${
-            [`"${ctx.version}"`, ...versions].map((version) =>
+            [ctx.version, ...versions].map((version) =>
               `"${version}"`
             ).join(', ')
           } ];\n`;
